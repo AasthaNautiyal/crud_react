6eec3690538a3fbb5a39b693053ecaa0e36b6020
Aastha Nautiyal <78968859+AasthaNautiyal@users.noreply.github.com>
2022-07-10 11:39:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
   };
   return (
     <div className="container">
-      <h1>CRUD App with Hooks</h1>
+      <h1>CRUD App</h1>
       <div className="flex-row">
         <div className="flex-large">
           <div>
