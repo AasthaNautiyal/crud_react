6a39b47c64e1e7bae5285c69dc9cb6ca67b1352b
unknown <nautiyalaastha33@gmail.com>
2022-07-24 23:13:38 +0530

    React-Forms

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,74 +1,59 @@
 import React, { useContext } from "react";
-import { useState } from "react";
 import { UserContext,user} from "../context/userContext";
-import { useEffect } from "react";
 import nextId from "react-id-generator";
+import { ReactForm } from 'react-forms';
 import "../App.css";
 const Form = () => {
-    const initialData={name:"",id:"",age:0,gender:""};
-    const [user, setUser] = useState<user>(initialData);
-    const {addUser,updateUser,editUser,editinguser}=useContext(UserContext);
-    const handleChange = (e: { target: { name: string; value: string; }; }) => {
-      const { name, value } = e.target;
-      setUser(prev => {
-          return ({
-              ...prev,
-              [name]: value
-          })
-      })
-  }
-  useEffect(()=>{
-    setUser(editinguser||initialData)
-  },[editinguser])
+  const {addUser,updateUser,editinguser,editUser}=useContext(UserContext);
+  const myConfig = [
+    {
+        type: 'text',
+        valueKey: 'name',
+        fieldProps: { label: 'Name', fullWidth: true},
+    },
+        {
+            type: 'text',
+            valueKey: 'age',
+            fieldProps: { label: 'Age',fullWidth:true},
+        },
+        {
+            type: 'select',
+            valueKey: 'gender',
+            fieldProps: { label:'Gender',
+                options: [
+                    { name: 'Male', value: 'Male' },
+                    { name: 'Female', value: 'Female' },
+                    { name: 'Others', value: 'Others' },
+                ],
+                styles : { width : '100%' }
+            },
+        },
+]
+const myInitialValues = editinguser || { name: '', age:'', gender: '', id: '' }
+
     return (
-      <div>
-        <form
-          onSubmit={e=>{
-            e.preventDefault();
-            if(!user.id)
-            { 
-              addUser({...user,id:nextId()})
-            }
-            else
-            {
-              updateUser(user)
-              editUser(undefined)
-            }
-            setUser({
-              name: "",
-              age: 0,
-              gender: "",
-              id: ""
-          })
-          }}
-        >
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={user.name}
-            onChange={handleChange} 
-            placeholder="Name"
-          />
-          <input
-            type="number"
-            name="age"
-            id="age"
-            value={user.age}
-            onChange={handleChange}
-            placeholder="Age"
-          />
-          <select name="gender" id="gender" value={user.gender} onChange={handleChange}> 
-          <option value="">Gender</option>
-          <option value="Female">Female</option>
-          <option value="Male">Male</option>
-          <option value="Others">Others</option>
-          </select>
-          <button>{editinguser?"Update":"Add"}</button>
-        </form>
-      </div>
-    );
-  };
-  
+        <div>
+            <div>
+                <ReactForm
+                    enableReinitialize
+                    config={myConfig}
+                    initialValues={myInitialValues}
+                    onSubmit={(values: Partial<user>, helper: { resetForm: () => {} }
+                    ) => {
+                        if (!values.id)
+                            addUser({ ...values, id: nextId() } as user)
+                        else
+                            {updateUser(values as user);
+                            editUser(undefined);
+                            }
+                        helper.resetForm();
+                        console.log(helper);
+                    }} formId={''}
+                    actionConfig={{ submitButtonProps:{} }}
+                />
+            </div>
+        </div>
+    )}
+
   export default Form;
   
